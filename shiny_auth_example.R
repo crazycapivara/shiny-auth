5b63d8d597c00f269c2b0b1f5c3331fba0bd2f85
skuethe <crazycapivara@gmail.com>
2017-02-08 19:07:33 +0100

    Add basic auth example

--- shiny_auth_example.R
+++ shiny_auth_example.R
@@ -1,11 +1,32 @@
 library(shiny)
+library(magrittr)
+
+.appv <- reactiveValues(logged_in = FALSE)
 
 view <- fluidPage(
-  h1("Welcome")
+  h1("itchy ibex"),
+  actionButton("in_and_out", "sign in or out"),
+  uiOutput("secret"),
+  style = "font-family: ubuntu;"
 )
 
 controller <- function(input, output){
+  observeEvent(input$in_and_out, {
+    print(.appv$logged_in)
+    .appv$logged_in %<>% ifelse(FALSE, TRUE)
+  })
   
+  output$secret <- renderUI({
+    if(.appv$logged_in){
+      list(
+        tableOutput("main")
+      )
+    } 
+  })
+  
+  output$main <- renderTable({
+      quakes[1:5, ]
+  })
 }
 
 shinyApp(view, controller)
